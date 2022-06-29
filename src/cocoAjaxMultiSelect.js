eabc3caa090ec0d58117bc4d31264a360d152adb
coco <squarehacker@kakao.com>
2022-06-30 00:44:04 +0900

    2022-06-30 00:44:04 업데이트!!!

--- src/cocoAjaxMultiSelect.js
+++ src/cocoAjaxMultiSelect.js
@@ -40,7 +40,7 @@
 			let value = $(this.element).val();
 			let multiple = $(this.element).attr('multiple');
 			$(this.element).after("<aside for='"+id+"'></aside>");
-			$("aside[for='"+id+"']").after("<figure for='"+id+"'></figure>");
+			$("aside[for='"+id+"']").after("<figure for='"+id+"'></s>");
 			if(multiple == 'multiple'){
 				if(value != ''){
 					$("s[for='"+id+"']").text(value.split(',').length);
