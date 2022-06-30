24207d16ca2a7d78cf872d0ecb45a1ec76327381
coco <squarehacker@kakao.com>
2022-06-30 11:23:04 +0900

    2022-06-30 11:23:04 업데이트!!!

--- src/cocoAjaxMultiSelect.js
+++ src/cocoAjaxMultiSelect.js
@@ -412,6 +412,9 @@
 		checkboxControl:function(){
 			let _this = this;
 			let multiple = this.$element.attr('multiple');
+			$(".ajaxselect_detail[for='"+_this.$element.attr('id')+"'] > li > input").change(function(){
+				console.log('test!');
+			});
 			$('html').on('change.cocoAjaxMultiSelect',".ajaxselect_detail[for='"+this.$element.attr('id')+"'] > li > input",function(){
 				let value = $(this).next('aside').text();
 				if($(this).is(":checked")){
