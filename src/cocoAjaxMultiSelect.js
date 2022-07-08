8ef97c1844df5b93fba432573c7ff3ec1b11d79d
coco <squarehacker@kakao.com>
2022-07-08 14:38:03 +0900

    2022-07-08 14:38:03 업데이트!!!

--- src/cocoAjaxMultiSelect.js
+++ src/cocoAjaxMultiSelect.js
@@ -44,10 +44,13 @@
 			let all_element = $._data($(document)[0],'events');
 			for(let event in all_element){
 				for(let e=0; e<all_element[event].length; e++){
-					console.log(all_element[event][e].selector);
-					if(all_element[event][e].selector.indexOf(id) !== -1){
-						console.log(id);
+					if(all_element[event][e].selector !== undefined){
+						console.log(all_element[event][e].selector);
+						if(all_element[event][e].selector.indexOf(id) !== -1){
+							console.log(id);
+						}
 					}
+					
 					if(all_element[event][e].namespace == 'cocoAjaxMultiSelect'){
 						document.removeEventListener(event, all_element[event][e].handler);
 					}
