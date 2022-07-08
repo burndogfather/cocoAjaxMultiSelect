9eb2a9c93905c4f11a0dca0a4e93bced024dc28f
coco <squarehacker@kakao.com>
2022-07-08 14:35:03 +0900

    2022-07-08 14:35:03 업데이트!!!

--- src/cocoAjaxMultiSelect.js
+++ src/cocoAjaxMultiSelect.js
@@ -44,6 +44,7 @@
 			let all_element = $._data($(document)[0],'events');
 			for(let event in all_element){
 				for(let e=0; e<all_element[event].length; e++){
+					console.log(all_element[event][e]);
 					if(all_element[event][e].namespace == 'cocoAjaxMultiSelect'){
 						document.removeEventListener(event, all_element[event][e].handler);
 					}
