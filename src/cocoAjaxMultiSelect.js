2e9c033b9b203b7a13a03582cac58c9b2c167424
coco <squarehacker@kakao.com>
2022-11-08 21:45:03 +0900

    2022-11-08 21:45:03 업데이트!!!

--- src/cocoAjaxMultiSelect.js
+++ src/cocoAjaxMultiSelect.js
@@ -573,7 +573,7 @@
 					multiple = false;
 				}
 				let searchtext = $(_this.element).val();
-				if(focus === 'on' && searchtext.length > 0){
+				if(focus === 'on'){
 					_this.settings['ajaxCode'](searchtext, page, _this.settings['pageUnit']).then((data)=>{
 						
 						_this.detailshow(id, data, multiple, $(_this.element).outerWidth()-30, $(_this.element).position().top+32, $(_this.element).position().left);
