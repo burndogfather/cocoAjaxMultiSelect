d3542d6638b0b24746e0b7d08a127c826a7d4c93
coco <squarehacker@kakao.com>
2024-03-26 13:53:03 +0900

    2024-03-26 13:53:03 업데이트!!!

--- src/cocoAjaxMultiSelect.origin.js
+++ src/cocoAjaxMultiSelect.origin.js
@@ -354,7 +354,9 @@
 						);
 						let overheight = scrollHeight + document.documentElement.scrollHeight - document.documentElement.clientHeight;
 						
-						$("#"+id+"[type='cocoAjaxMultiSelect']").before("<div for='"+id+"' class='ajaxselect_over' style='height:"+overheight+"px;'></div>");
+						$("label[for='"+id+"'][ajaxselect_label]").after("<div for='"+id+"' class='ajaxselect_over' style='height:"+overheight+"px;'></div>");
+						//$("#"+id+"[type='cocoAjaxMultiSelect']").before("<div for='"+id+"' class='ajaxselect_over' style='height:"+overheight+"px;'></div>");
+						//setTimeout(() => $("#"+id+"[type='cocoAjaxMultiSelect']").focus(), 100);
 						
 						_this.detailshow(id, data, multiple, $(this).outerWidth()-30, $(this).position().top+32, $(this).position().left);
 
